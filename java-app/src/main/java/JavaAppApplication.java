ad0e93d9af7e7b0c69f7ce704a3ffaa72e1192a7
Spencer Hinrichs <spencer.hinrichs@buckle.com>
2019-05-09 12:40:05 -0500

    Made both front end and back end modules build into single .jar

---
+++ java-app/src/main/java/JavaAppApplication.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAppApplication.class, args);
+    }
+}
