86555d578f93e6e971f865bd261004d9d721ac75
Stacy Peace <stacy.peace@buckle.com>
2019-05-21 13:38:03 -0500

    Testing out mysql configuration

--- server/src/main/java/toxiproxy/config/ApplicationConfig.java
+++ server/src/main/java/toxiproxy/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package toxiproxy.backup.config;
+package toxiproxy.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
