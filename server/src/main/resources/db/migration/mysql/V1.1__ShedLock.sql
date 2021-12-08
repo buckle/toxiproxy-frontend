a3daea3ad1adc190eb2c8ab850e0a1667af0a633
jonathank <jonathan.kauk@buckle.com>
2021-12-08 15:27:10 -0600

    EC-13360 Converted server from using Flyway to Liquibase for db changes

--- server/src/main/resources/db/migration/mysql/V1.1__ShedLock.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE shedlock (
-    name VARCHAR(64) NOT NULL,
-    lock_until TIMESTAMP(3) NULL,
-    locked_at TIMESTAMP(3) NULL,
-    locked_by VARCHAR (255) NOT NULL,
-    PRIMARY KEY (name)
-);
